7606b517c9cd4259156e0b94912e96d8864914d6
Patrick Marsceill <patrick.marsceill@gmail.com>
2018-01-26 10:15:35 -0500

    Make it work and add some logging

---
+++ extension/background.js
@@ -0,0 +1,43 @@
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+  let data = msg
+  console.log(data)
+
+  // Handle the form submit via JS
+  var XHR = new XMLHttpRequest()
+  var urlEncodedData = ""
+  var urlEncodedDataPairs = []
+  var name
+
+  XHR.withCredentials = true
+  // Turn the data object into an array of URL-encoded key/value pairs.
+  for(name in data) {
+    urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]))
+  }
+
+  // Combine the pairs into a single string and replace all %-encoded spaces to
+  // the '+' character; matches the behaviour of browser form submissions.
+  urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+')
+
+  // Define what happens on successful data submission
+  XHR.addEventListener('load', function(event) {
+    console.log("Successful post to Emo server...")
+    console.log("Response from server:")
+    console.log(XHR.response)
+  });
+
+  // Define what happens in case of error
+  XHR.addEventListener('error', function(event) {
+    console.log("Something didn't work right...")
+  });
+
+  // Set up our request
+  XHR.open('POST', 'https://seemly-cello.glitch.me/react')
+
+  // Add the required HTTP header for form data POST requests
+  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+  // Finally, send our data.
+  console.log("Sending data to Emo server...")
+  console.log(urlEncodedData)
+  XHR.send(urlEncodedData)
+})
--- extension/inject.js
+++ extension/inject.js
@@ -1,6 +1,8 @@
-console.log("DOGS")
 
 let pops = document.getElementsByClassName("js-add-reaction-popover")
+let userContainer = document.getElementsByClassName("header-nav-current-user")[0]
+let user = userContainer.getElementsByClassName("css-truncate-target").innerHTML
+
 for (let i = 0; i < pops.length; i++) {
   let pop = pops[i]
   let form = pop.getElementsByClassName("js-pick-reaction")[0]
@@ -12,32 +14,50 @@ for (let i = 0; i < pops.length; i++) {
   let emoReactions = emoForm.getElementsByClassName("add-reactions-options")[0]
   let emoReactionButtons = emoReactions.getElementsByClassName("js-reaction-option-item")
 
+  emoForm.setAttribute("action", "")
   emoForm.classList.add("mt-4")
   emoDesc.innerHTML = "Add to your private diary"
 
+
   emoReactionButtons[0].innerHTML = "<g-emoji alias=\"rage\" class=\"emoji-result\" fallback-src=\"https://assets-cdn.github.com/images/icons/emoji/unicode/1f621.png\" ios-version=\"6.0\">😡</g-emoji>"
-  emoReactionButtons[0].setAttribute("value", "RAGE react" )
+  emoReactionButtons[0].setAttribute("value", "😡" )
   emoReactionButtons[0].setAttribute("data-reaction-label", "Rage" )
 
   emoReactionButtons[1].innerHTML = "<g-emoji alias=\"angry\" class=\"emoji-result\" fallback-src=\"https://assets-cdn.github.com/images/icons/emoji/unicode/1f620.png\" ios-version=\"6.0\">😠</g-emoji>"
-  emoReactionButtons[1].setAttribute("value", "ANGRY react" )
+  emoReactionButtons[1].setAttribute("value", "😠" )
   emoReactionButtons[1].setAttribute("data-reaction-label", "Angry" )
 
   emoReactionButtons[2].innerHTML = "<g-emoji alias=\"dissapointed\" class=\"emoji-result\" fallback-src=\"https://assets-cdn.github.com/images/icons/emoji/unicode/1f61e.png\" ios-version=\"6.0\">😞</g-emoji>"
-  emoReactionButtons[2].setAttribute("value", "DISSAPOINTED react" )
+  emoReactionButtons[2].setAttribute("value", "😞" )
   emoReactionButtons[2].setAttribute("data-reaction-label", "Dissapointed" )
 
   emoReactionButtons[3].innerHTML = "<g-emoji alias=\"relieved\" class=\"emoji-result\" fallback-src=\"https://assets-cdn.github.com/images/icons/emoji/unicode/1f60c.png\" ios-version=\"6.0\">😌</g-emoji>"
-  emoReactionButtons[3].setAttribute("value", "RELIEVED react" )
+  emoReactionButtons[3].setAttribute("value", "😌" )
   emoReactionButtons[3].setAttribute("data-reaction-label", "Relieved" )
 
   emoReactionButtons[4].innerHTML = "<g-emoji alias=\"grinning\" class=\"emoji-result\" fallback-src=\"https://assets-cdn.github.com/images/icons/emoji/unicode/1f600.png\" ios-version=\"6.0\">😀</g-emoji>"
-  emoReactionButtons[4].setAttribute("value", "GRINNING react" )
+  emoReactionButtons[4].setAttribute("value", "😀" )
   emoReactionButtons[4].setAttribute("data-reaction-label", "Grinning" )
 
   emoReactionButtons[5].innerHTML = "<g-emoji alias=\"heart_eyes\" class=\"emoji-result\" fallback-src=\"https://assets-cdn.github.com/images/icons/emoji/unicode/1f60d.png\" ios-version=\"6.0\">😍</g-emoji>"
-  emoReactionButtons[4].setAttribute("value", "HEART_EYES react" )
-  emoReactionButtons[4].setAttribute("data-reaction-label", "Heart eyes" )
+  emoReactionButtons[5].setAttribute("value", "😍" )
+  emoReactionButtons[5].setAttribute("data-reaction-label", "Heart eyes" )
+
+
+  Array.prototype.forEach.call(emoReactionButtons, function(el, i){
+    el.classList.add("js-emo-reaction")
+    el.addEventListener("click", function( event ) {
+      event.preventDefault()
+      let reaction = el.getAttribute("value")
+      let url = location
+      let object_id = el.closest(".js-comment").id
+      let actor = user
+      let author = el.closest(".timeline-comment-avatar").firstChild.attribute("href")
+      let text = "test"
+
+      chrome.runtime.sendMessage({object_id: object_id, url: url, reaction: reaction, text: text, actor: actor, author: author});
+    })
+  })
 
   // Add emoForm to the DOM
   pop.appendChild(emoForm)
